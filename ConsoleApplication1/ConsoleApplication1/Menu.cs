f751e514659511adfcd4eb6e26442b9897303789
vladeslav <vladeslav0086@gmail.com>
2017-09-28 20:42:13 +0300

    using menu in main

---
+++ ConsoleApplication1/ConsoleApplication1/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Menu : IMenu
+    {
+        public string Add(IPeople person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LoadFromFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Remove(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaveToFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Sort(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
