8e6059307be172fcc9ea46585966ae25bbc3bd3d
vladeslav <vladeslav0086@gmail.com>
2017-09-28 19:50:44 +0300

    Interfaces

---
+++ ConsoleApplication1/ConsoleApplication1/IMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    interface IMenu
+    {
+        void Add(IPeople person);
+        void Remove(IPeople person);
+        void Sort(object parameter);
+        void Print();
+        void SaveToFile(List<IPeople> people);
+        List<IPeople> LoadFromFile(string fileName);
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/IPeople.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    interface IPeople
+    {
+        string Name{ get; set; }
+        string Phone { get; set; }
+        string Address { get; set; }
+    }
+}
