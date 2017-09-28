101d8b3c1dcb066dff4b36d57ac14f9959083f66
student <student@K202-15>
2017-09-28 21:32:06 +0300

    Fix Remove

--- ConsoleApplication1/ConsoleApplication1/Menu.cs
+++ ConsoleApplication1/ConsoleApplication1/Menu.cs
@@ -36,7 +36,14 @@ namespace ConsoleApplication1
 
         public string Remove(string name)
         {
-            throw new NotImplementedException();
+            IPeople person = People.Find(p => p.Name == name);
+            if (person != null)
+            {
+                People.Remove(person);
+                return "removed";
+            }
+            else
+                return "not found";
         }
 
         public void Remove(IPeople person)
